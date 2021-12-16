efbee9b1093cdd1063dc493b6d6598a975e14eeb
Chitsanupong <first30952@gmail.com>
2021-12-16 12:03:23 +0700

    feat: field visited

--- src/components/EmailForm.jsx
+++ src/components/EmailForm.jsx
@@ -1,34 +1,80 @@
 import react from 'react';
 import { useFormik } from 'formik';
 
+const initialValues = {
+  name: '',
+  email: '',
+};
+
+const onSubmit = (values) => {
+  console.log(values);
+};
+
+const validate = (values) => {
+  let errors = {};
+
+  if (!values.name) {
+    errors.name = 'Required';
+  }
+
+  if (!values.email) {
+    errors.email = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+    errors.email = 'Invalid email address';
+  }
+  return errors;
+};
+
 const EmailForm = () => {
   const formik = useFormik({
-    initialValues: {
-      name: '',
-      email: '',
-    },
-    onSubmit: (values) => {
-      console.log(values);
-    },
+    initialValues: initialValues,
+    onSubmit: onSubmit,
+    validate: validate,
   });
+
+  const isNameValidated = formik.touched.name && formik.errors.name;
+  const isEmailValidated = formik.touched.email && formik.errors.email;
 
   return (
     <form onSubmit={formik.handleSubmit}>
       <h2>Email Form</h2>
-      <label htmlFor="name">Name</label>
-      <input
-        type="text"
-        name="name"
-        onChange={formik.handleChange}
-        value={formik.values.name}
-      />
-      <label htmlFor="name">Email</label>
-      <input
-        type="email"
-        name="email"
-        onChange={formik.handleChange}
-        value={formik.values.email}
-      />
+      <section>
+        <label htmlFor="name" className={isNameValidated ? 'error-label' : ''}>
+          Name
+        </label>
+        <input
+          type="text"
+          name="name"
+          className={isNameValidated ? 'error-input' : ''}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.name}
+        />
+        {isNameValidated ? (
+          <p className="error-msg">{formik.errors.name}</p>
+        ) : null}
+      </section>
+
+      <section>
+        <label
+          htmlFor="email"
+          className={isEmailValidated ? 'error-label' : ''}
+        >
+          Email
+        </label>
+        <input
+          type="email"
+          name="email"
+          className={isEmailValidated ? 'error-input' : ''}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.email}
+        />
+        {isEmailValidated ? (
+          <p className="error-msg">{formik.errors.email}</p>
+        ) : null}
+      </section>
+
       <button type="submit">Submit</button>
     </form>
   );
