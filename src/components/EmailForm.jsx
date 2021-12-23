0fcac2b365897ca0e1e48a6ba921f666038dc6da
Chitsanupong <first30952@gmail.com>
2021-12-23 21:32:26 +0700

    feat: render other component inside formik

--- src/components/EmailForm.jsx
+++ src/components/EmailForm.jsx
@@ -1,9 +1,12 @@
 import { Formik, Form, Field, ErrorMessage } from 'formik';
+import TextError from './TextError';
 import * as yup from 'yup';
 
 const initialValues = {
   name: '',
   email: '',
+  comment: '',
+  address: '',
 };
 
 const onSubmit = (values) => {
@@ -26,14 +29,40 @@ const EmailForm = () => {
         <h2>Email Form</h2>
         <section>
           <label htmlFor="name">Name</label>
-          <Field type="text" name="name" />
-          <ErrorMessage name="name" component="p" className="error-msg" />
+          <Field type="text" id="name" name="name" placeholder="Your Name" />
+          <ErrorMessage name="name" component={TextError} />
         </section>
 
         <section>
           <label htmlFor="email">Email</label>
-          <Field type="email" name="email" />
-          <ErrorMessage name="email" component="p" className="error-msg" />
+          <Field
+            type="email"
+            id="email"
+            name="email"
+            placeholder="demo@example.com"
+          />
+          <ErrorMessage name="email">
+            {(error) => <div className="error-label">{error}</div>}
+          </ErrorMessage>
+        </section>
+
+        <section>
+          <label htmlFor="comment">Comment</label>
+          <Field id="comment" name="comment" as="textarea" />
+        </section>
+
+        <section>
+          <label htmlFor="address">Address</label>
+          <Field name="address">
+            {({ field, form, meta }) => {
+              return (
+                <div>
+                  <input type="text" id="address" {...field} />
+                  {meta.touched && meta.error && <span>{meta.error}</span>}
+                </div>
+              );
+            }}
+          </Field>
         </section>
 
         <button type="submit">Submit</button>
---
+++ src/components/TextError.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TextError = (props) => {
+  return <div className="error-label">{props.children}</div>;
+};
+
+export default TextError;
